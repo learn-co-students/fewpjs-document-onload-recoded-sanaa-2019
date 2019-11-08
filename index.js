dccb76753fe50382042d8d8e80a5efbe834991d3
mustafa-al-shaheri <mustafa.alshaheri@gmail.com>
2019-11-08 11:02:53 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let para=document.getElementById('text').innerHTML="This is really cool!"
+    console.log("The DOM has loaded");
+  });
