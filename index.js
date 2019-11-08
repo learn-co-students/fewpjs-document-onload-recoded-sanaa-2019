911b921ba8192d0f7ec43cb5f88f949201adc19d
mustafa-al-shaheri <mustafa.alshaheri@gmail.com>
2019-11-08 11:06:15 -0800

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
-    let para=document.getElementById('text').innerHTML="This is really cool!"
-    console.log("The DOM has loaded");
+    updateDom();
   });
+
+  function updateDom(){
+    let para=document.getElementById('text').innerHTML="This is really cool!"
+
+  }
